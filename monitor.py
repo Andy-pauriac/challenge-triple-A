d3846771820baa1aef19c9f8f8449c55250240f3
YkaRUs Rodriguez <axel.rodriguez@laplateforme.com>
2025-12-10 08:46:10 +0100

    cleaning some details and adding font to the text

--- monitor.py
+++ monitor.py
@@ -34,11 +34,10 @@ def index():
         status = i.status
 
     # SECTION PROCESSUS 
-    cores = psutil.cpu_count()
     for p in psutil.process_iter(): p.cpu_percent()
     time.sleep(0.1)
 
-    procs = [(p.cpu_percent()/cores, p.pid, p.name())
+    procs = [(p.cpu_percent()/cpu_cores, p.pid, p.name())
             for p in psutil.process_iter() if p.pid not in (0,4)]
     
     procslist = sorted(procs, reverse=True)[:3]
